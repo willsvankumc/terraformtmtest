a6eaca966235bff56b1a94b0f5d0794c8260d0c2
Wills Van Doorn <54042608+willsvankumc@users.noreply.github.com>
2022-11-08 11:57:11 -0600

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "Harshul_Routhu" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+}
